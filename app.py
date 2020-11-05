8fd714219e735653f11ab2c8a5eb32583f51dbfd
parulchourasia90 <71695217+parulchourasia90@users.noreply.github.com>
2020-11-05 16:02:54 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Welcome !"
+	
+	
+@app.route('/how are you')
+def hello():
+    return 'I am good , how about you?'
+	
+	
+if __name__ == "_main_":
+     app.run(host="0.0.0.0", port=8080)
